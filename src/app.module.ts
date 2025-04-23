21036ad69bdef858661368150ed27bc36a7cff50
VillafuerteJonathan <86273066+VillafuerteJonathan@users.noreply.github.com>
2025-04-23 14:58:25 -0500

    coneccion a la base
    se conecta a la base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '0000',
+      database: 'DB_Cementerio',
+      autoLoadEntities: true,
+      synchronize: true, // en producción debe estar en false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    if (dataSource.isInitialized) {
+      console.log('✅ Conexión a la base de datos establecida');
+    } else {
+      console.log('❌ No se pudo conectar a la base de datos');
+    }
+  }
+
+}
