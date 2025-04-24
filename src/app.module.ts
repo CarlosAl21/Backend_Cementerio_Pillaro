9fc99635e0ac12dc596156240631061cbc7f7b31
CarlosAl21 <calvarado2731@uta.edu.ec>
2025-04-24 15:24:08 -0500

    Se agrego la conexion a la bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,33 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CementerioModule } from './cementerio/cementerio.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { Cementerio } from './cementerio/entities/cementerio.entity';
 
 @Module({
-  imports: [UserModule, CementerioModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      schema: process.env.DB_SCHEMA, // Asegúrate de que esta línea esté correcta
+      autoLoadEntities: true,
+      entities: [
+        User,
+        Cementerio,
+      ],
+      synchronize: true,
+    }),
+    UserModule, 
+    CementerioModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
