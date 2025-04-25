f02541c8efe688adcea40766575a1fc3ef46bfe1
Dalembert31 <dalembertbravo2@gmail.com>
2025-04-25 07:26:24 -0500

    correccion de errores en la importancion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { PersonasModule } from './personas/personas.module';
 import { PropietariosNichosModule } from './propietarios-nichos/propietarios-nichos.module';
 
 @Module({
-  imports: [PersonasModule, PropietariosNichosModule],
+  imports: [
+   
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', 
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'pillaro',
+      autoLoadEntities: true, 
+      synchronize: true,      
+    }),
+
+    
+    PersonasModule,
+    PropietariosNichosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-
--- src/propietarios-nichos/dto/update-propietarios-nicho.dto.ts
+++ src/propietarios-nichos/dto/update-propietarios-nicho.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropietarioNichoDto } from './create-propietario-nicho.dto';
+import { CreatePropietarioNichoDto } from './create-propietarios-nicho.dto';
 
 export class UpdatePropietarioNichoDto extends PartialType(CreatePropietarioNichoDto) {}
