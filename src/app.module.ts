ffa9d748cb85a4fae3f2898e289ba51672eb1271
CarlosAl21 <calvarado2731@uta.edu.ec>
2025-04-24 19:59:40 -0500

    No tocar main!!!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0000',
-      database: 'DB_Cementerio',
-      autoLoadEntities: true,
-      synchronize: true, // en producción debe estar en false
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private dataSource: DataSource) {
-    if (dataSource.isInitialized) {
-      console.log('✅ Conexión a la base de datos establecida');
-    } else {
-      console.log('❌ No se pudo conectar a la base de datos');
-    }
-  }
-
-}
+export class AppModule {}
